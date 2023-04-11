8fdf32766430cf306b3d2aa1333497bd876dc653
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-04-11 14:35:07 -0300

    secund commit

--- Avali-o/script.js
+++ Avali-o/script.js
@@ -1,21 +1,22 @@
-function converter(){
-        var valor = parseFloat(document.getElementById("valor").value);
-        var converter = document.getElementById("converter").value;
-        var resultado;
-    
-        switch (converter) {
-            case "Quilometro":
-                resultado = valor / 1000
-                break;
-            case "Centimetro":
-                resultado = valor * 100
-                break;
-            case "Milimetro":
-                resultado = valor * 1000
-                break;
-            case "Milha":
-                resultado = valor / 1609
-                break;
-        }
+function calculo() {
+    var valor = parseFloat(document.getElementById("valor").value);
+    var converter = document.getElementById("converter").value;
+    var resultado;
+
+    switch (converter) {
+        case "euro":
+            resultado = 5.46
+            break;
+        case "dolar":
+            resultado =  5.01
+            break;
+        case "libra":
+            resultado =  6.21
+            break;
+    }
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = "O resultado em real é R$" + valorEmReais
+    elementoValorConvertido.innerHTML = valorConvertido
 
 }
