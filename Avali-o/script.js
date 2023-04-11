16fe28fc50bf683b526a542da0608a7c5608cb6a
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-04-11 15:17:03 -0300

    fourth  commit

--- Avali-o/script.js
+++ Avali-o/script.js
@@ -1,11 +1,27 @@
 function Converter() {
-    var valorCotacao = document.getElementById("cotacao").value;
-    var valorDolar = document.getElementById("valor").value;
+    var valorCotacaoDolar = document.getElementById("dolar").value;
+    var valorCotacaoEuro = document.getElementById("euro").value;
+    var valorCotacaoLibra = document.getElementById("libra").value;
 
-    var valorEmReais = valorDolar * valorCotacao
+    var valor = document.getElementById("valor").value;
+    
+
+    var valorEmReais = valor * valorCotacaoLibra;
+    var valorEmReaisEuro = valor * valorCotacaoEuro;
+    var valorEmReaisDolar = valor * valorCotacaoDolar;
     var valorEmReais = valorEmReais.toFixed(2)
-
+    
     var elementoValorConvertido = document.getElementById("valorConvertido")
+  
     var valorConvertido = "O resultado em real é R$" + valorEmReais
     elementoValorConvertido.innerHTML = valorConvertido
-}
+    var elementoValorConvertido = document.getElementById("valorConvertido2")
+  
+    var valorConvertido = "O resultado em real é R$" + valorEmReaisEuro
+    elementoValorConvertido.innerHTML = valorConvertido
+    var elementoValorConvertido = document.getElementById("valorConvertido3")
+  
+    var valorConvertido = "O resultado em real é R$" + valorEmReaisDolar
+    elementoValorConvertido.innerHTML = valorConvertido
+  }
+  
